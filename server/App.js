0534963c9dc41ec7f9e27e6b49ce9fa46cf9a9b6
Maurice Christopher <mchristo@student.42.us.org>
2019-04-15 20:09:21 -0700

    dummy schema built

--- server/App.js
+++ server/App.js
@@ -1,11 +1,12 @@
 const express = require('express')
 // One endpoint to rule them all
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
 app.use('/graphql', graphqlHTTP({
-  
+  schema // same as saying schema: schema... Thanks es6
 }));
 
 app.listen(4242, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,19 @@
 const graphql = require('graphql')
+const _ = require('lodash')
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+// dummy data
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id:'1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id:'2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id:'3' }
+]
 
 //function that takes in an object and it's desired fields
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
+    // pretty straightforward
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
@@ -14,6 +22,7 @@ const BookType = new GraphQLObjectType({
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
+  // defines the route to take into the graph/api
   fields: {
     book: {
       type: BookType,
@@ -21,8 +30,13 @@ const RootQuery = new GraphQLObjectType({
       // looks like book(id: '123'){name genre} from the front-end
       resolve(parent, args) {
         // code to get data from the db / other source
-        
+        return _.find(books, { id: args.id }) // looks through books array and returns all items with the matching id
       }
     }
   }
 })
+
+// defining the schema to be called from the frontend
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
